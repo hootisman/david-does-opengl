7af8f1db31054d828388ff4ee44e5fed54211ee6
hootisman <david1zalewski@gmail.com>
2023-06-25 02:14:56 -0400

    vertex shader

--- main.cpp
+++ main.cpp
@@ -6,11 +6,20 @@
 
 using namespace std;
 
-void framebuffer_size_cb(GLFWwindow *, int, int);
+void renderTriangle(GLFWwindow *);
 void processInput(GLFWwindow *);
+void framebuffer_size_cb(GLFWwindow *, int, int);
 
 static int WIDTH = 800;
 static int HEIGHT = 600;
+
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
 
 int main() {
 
@@ -42,6 +51,8 @@ int main() {
     glClearColor(0.1f, 0.5f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    renderTriangle(window);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -49,7 +60,18 @@ int main() {
   glfwTerminate();
   return 0;
 }
+void renderTriangle(GLFWwindow *window) {
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
 
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+}
 void processInput(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, true);
