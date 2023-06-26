9e62dfc14a97050e6e0ad59ae54054c4f8cb8ca9
hootisman <david1zalewski@gmail.com>
2023-06-26 13:01:36 -0400

    working triangle

--- main.cpp
+++ main.cpp
@@ -54,41 +54,13 @@ int main() {
   glfwSetFramebufferSizeCallback(window, framebuffer_size_cb);
 
   unsigned int shaderProgram;
-  /* newShaderProgram(shaderProgram); */
-
-  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  glCompileShader(vertexShader);
-
-  checkShaderCompilation(vertexShader, "VERTEX");
-
-  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-
-  checkShaderCompilation(fragmentShader, "FRAGMENT");
-
-  shaderProgram = glCreateProgram();
-
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-
-  int success;
-  char log[512];
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-  if (!success) {
-    glGetProgramInfoLog(shaderProgram, 512, NULL, log);
-    cout << "ERROR LINKING PROGRAM SHADERS\n" << log << endl;
-  }
-
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+  newShaderProgram(shaderProgram);
 
   float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
 
   unsigned int VAO, VBO;
   glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
   glGenBuffers(1, &VBO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
